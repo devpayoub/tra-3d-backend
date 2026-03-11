8e07cee41fa9a4102b0822caef5c997067f44560
Ayoub Trabelsi <143298746+devpayoub@users.noreply.github.com>
2026-03-11 05:34:34 +0100

    change username to name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# Collect static files (required for Admin panel with DEBUG=False)
 python manage.py collectstatic --noinput
-
-# Run migrations
 python manage.py migrate --noinput
 
-# Create superuser automatically if it doesn't exist
-python manage.py createsuperuser \
-    --noinput \
-    --name $DJANGO_SUPERUSER_NAME \
-    --email $DJANGO_SUPERUSER_EMAIL || true
+# Create superuser only if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Start Gunicorn
+username = "$DJANGO_SUPERUSER_NAME"
+email = "$DJANGO_SUPERUSER_EMAIL"
+password = "$DJANGO_SUPERUSER_PASSWORD"
+
+if not User.objects.filter(name=name).exists():
+    print("Creating admin user...")
+    User.objects.create_superuser(name, email, password)
+else:
+    print("Admin user already exists.")
+END
+
 exec gunicorn --bind 0.0.0.0:8000 authAPI.wsgi:application
